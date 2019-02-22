9f8f9460745736a7dfbed33414887aca25f1c534
unknown <crmb.cnet@gmail.com>
2019-02-22 13:53:37 -0600

    load zauru 2

--- src/app/services/fetch.js
+++ src/app/services/fetch.js
@@ -2,17 +2,16 @@ import axios from 'axios';
 
 export const turnos = async () => {
 	try {
-
 		const turnos = await axios.get(process.env.GATSBY_ZAURU_URL, { headers:{ 'Accept': 'application/json',
         'Content-Type': 'application/json',
         'X-User-Email': process.env.GATSBY_ZAURU_USER,
         'X-User-Token': process.env.GATSBY_ZAURU_TOKEN,} });
+        console.error(turnos);
 		return {
 			statusCode: 200,
 			body: JSON.stringify(turnos)
 		};
-	} catch (error) {
-		console.error(error);
+	} catch (error) {		
 		return {
 			statusCode: 502,
 			body: JSON.stringify(error)

3565c7ef798176cc51a84b427a680d08a46a11b7
Carlos Martínez <crmb.cnet@gmail.com>
2019-10-07 12:54:50 -0600

    first commit

---
+++ src/service/EditarSecuencia.js
@@ -0,0 +1,40 @@
+//@ts-check
+import axios from 'axios';
+import { headers, URLS } from '../utils/utils';
+import format from 'date-fns/format';
+import subHours from 'date-fns/sub_hours';
+import JSON from 'circular-json';
+
+const headersr = {
+	'Access-Control-Allow-Origin': '*',
+	'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
+	'Content-Type': 'application/json',
+	'Access-Control-Allow-Methods': '*',
+	'Access-Control-Max-Age': '2592000',
+	'Access-Control-Allow-Credentials': 'true',
+  };
+
+exports.handler = (event, context, callback) => {
+	if (event.httpMethod !== 'POST') {
+		return { statusCode: 405, body: 'Method Not Allowed!' };
+	}
+	console.log(event.body);
+	
+	let URL = 'https://dcgse.com/calendario_api/apiprod/editarsecuencia';
+	
+	return axios
+		.post(URL, event.body, { headers: headers })
+		.then((data) => {
+			console.log({ response: JSON.stringify(data.data) });
+			return {
+				statusCode: 200,
+				body: JSON.stringify(data.data),
+				headers:headersr,
+			};
+		})
+		.catch((error) => ({
+			statusCode: 422,
+			body: `Oops! Something went wrong. ${error}`,
+			headers:headersr,
+		}));
+};
---
+++ src/service/GetSecuencias.js
@@ -0,0 +1,32 @@
+//@ts-check
+import axios from 'axios';
+import { headers, URLS } from '../utils/utils';
+const URL = URLS.empleados;
+const headersr = {
+	'Access-Control-Allow-Origin': '*',
+	'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
+	'Content-Type': 'application/json',
+	'Access-Control-Allow-Methods': '*',
+	'Access-Control-Max-Age': '2592000',
+	'Access-Control-Allow-Credentials': 'true',
+  };
+//@ts-ignore
+exports.handler = async (event, context) => {
+	try {
+		//@ts-ignore
+		let id = event.queryStringParameters.id;
+		let { data } = await axios.get("https://dcgse.com/calendario_api/apiprod/secuencias?id="+id, { headers });
+		console.log(data)
+		return {
+			statusCode: 200,
+			body: JSON.stringify(data),
+			headers:headersr
+		};
+	} catch (error) {
+		console.error(error);
+		return {
+			statusCode: 502,
+			body: JSON.stringify(error)
+		};
+	}
+};
